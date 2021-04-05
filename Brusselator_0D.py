e23a6f645614fe1f762e2a400897537b86602840
quent142 <58600555+quent142@users.noreply.github.com>
2021-04-05 17:50:22 +0200

    Update Brusselator_0D.py

--- Brusselator_0D.py
+++ Brusselator_0D.py
@@ -1,18 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def func(A,B,u_0,v_0, t_f, N) :
-    h = t_f/N
+def func(A,B,u_0,v_0, t_f, h) :
     u = [u_0]
     v = [v_0]
     t = [0]
-    for i in range(N) :
+    der_u = []
+    der_v = []
+    while t[-1] < t_f :
         dudt = A + v[-1]*u[-1]**2 - B*u[-1] - u[-1]
+        der_u.append(dudt)
         dvdt = B*u[-1] - v[-1]*u[-1]**2
+        der_v.append(dvdt)
         u.append(u[-1] + h*dudt)
         v.append(v[-1] + h*dvdt)
         t.append(t[-1] + h)
-    return u,v,t
+    dudt = A + v[-1]*u[-1]**2 - B*u[-1] - u[-1]
+    der_u.append(dudt)
+    dvdt = B*u[-1] - v[-1]*u[-1]**2
+    der_v.append(dvdt)
+    return u,v,t,der_u,der_v
 
-a,b,t = func(1,2,0,0,100,1000)
+a,b,t,du,dv = func(1,2,0,0,100,0.1)
 plt.plot(t,a)
+plt.plot(t,du)
