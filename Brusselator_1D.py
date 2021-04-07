e4ca7fe96f6bf8f88ba11ae20667c11375c1a288
quent142 <58600555+quent142@users.noreply.github.com>
2021-04-07 11:45:41 +0200

    Update Brusselator_1D.py

--- Brusselator_1D.py
+++ Brusselator_1D.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import random as rd
 
 def dudt(u,v,A,B) :
     der_u = A + v*u**2 - B*u - u
@@ -14,10 +15,16 @@ k = 0.1
 A = 1
 B = 3
 D_u = 10
+N = int(50/h) + 1
 
-x = np.linspace(0,50,int(50/h) + 1)
-u = np.zeros(int(50/h) + 1)
-v = np.zeros(int(50/h) + 1)
+x = np.linspace(0,50,N)
+u = np.zeros(N)
+v = np.zeros(N)
+'''
+for i in range(N) :
+    u[i] = rd.uniform(0,5)
+    v[i] = rd.uniform(0,5)
+'''
 time = 10
 plt.xlim(0,1)
 plt.ylim(0,5)
