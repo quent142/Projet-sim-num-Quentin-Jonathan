def176711edc30f9bc29d7384a149a90bb1e95d3
Cariama <58600203+Cariama@users.noreply.github.com>
2021-04-07 10:24:35 +0200

    meilleur h et une question

--- Brusselator_0D.py
+++ Brusselator_0D.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def func(A,B,u_0,v_0, t_f, h) :
+def func(A,B,u_0,v_0, t_f, N) :#j'ai changer h en N
+    #le h peut causer des problème avec t (on peut se retrouver avec t[-1]>t_f), je change un peu le code pour que ça aille mieux,
+    #mais je me dis que ce serait peut-être mieux d'utiliser linspace
+    h = t_f/N #ici je défini h
     u = [u_0]
     v = [v_0]
     t = [0]
@@ -15,7 +18,7 @@ def func(A,B,u_0,v_0, t_f, h) :
         u.append(u[-1] + h*dudt)
         v.append(v[-1] + h*dvdt)
         t.append(t[-1] + h)
-    dudt = A + v[-1]*u[-1]**2 - B*u[-1] - u[-1]
+    dudt = A + v[-1]*u[-1]**2 - B*u[-1] - u[-1]#Est-ce vraiment utile de calculer ces dérivées?
     der_u.append(dudt)
     dvdt = B*u[-1] - v[-1]*u[-1]**2
     der_v.append(dvdt)
