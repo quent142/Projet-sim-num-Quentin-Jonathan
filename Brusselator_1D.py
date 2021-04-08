eb42cc3a2aac2a8e406f53a71107831784f886df
quent142 <58600555+quent142@users.noreply.github.com>
2021-04-08 16:26:42 +0200

    Update Brusselator_1D.py

--- Brusselator_1D.py
+++ Brusselator_1D.py
@@ -11,23 +11,23 @@ def dvdt(u,v,A,B) :
     return der_v
 
 h = 1
-k = 0.1
+k = 0.01
 A = 1
 B = 3
 D_u = 10
 N = int(50/h) + 1
+time = 30
 
 x = np.linspace(0,50,N)
 u = np.zeros(N)
 v = np.zeros(N)
-'''
+
 for i in range(N) :
     u[i] = rd.uniform(0,5)
     v[i] = rd.uniform(0,5)
-'''
-time = 10
-plt.xlim(0,1)
-plt.ylim(0,5)
+
+plt.xlim(0,50)
+plt.ylim(-1,5)
 line1, = plt.plot(x,u, label = 'composant u')
 line2, = plt.plot(x,v, label = 'composant v')
 plt.legend()
@@ -36,11 +36,16 @@ for i in range(int(time/k)) :
     u_d = np.append(u[0],u[: -1])
     v_g = np.append(v[1 :],v[-1])
     v_d = np.append(v[0],v[: -1])
-    u_ = u + k*(dudt(u,v,A,B) + D_u*(u_g - 2*u + u_d)/h**2)
-    v_ = v + k*(dvdt(u,v,A,B) + 10*(v_g - 2*v + v_d)/h**2)
-    u = u_
-    v = v_
+    K1 = k*(dudt(u,v,A,B) + D_u*(u_g - 2*u + u_d)/h**2)
+    J1 = k*(dvdt(u,v,A,B) + 10*(v_g - 2*v + v_d)/h**2)
+    K2 = k*(dudt(u + K1/2,v + J1/2,A,B) + D_u*(u_g - 2*u + u_d)/h**2)
+    J2 = k*(dvdt(u + K1/2,v + J1/2,A,B) + 10*(v_g - 2*v + v_d)/h**2)
+    K3 = k*(dudt(u + K2/2,v + J2/2,A,B) + D_u*(u_g - 2*u + u_d)/h**2)
+    J3 = k*(dvdt(u + K2/2,v + J2/2,A,B) + 10*(v_g - 2*v + v_d)/h**2)
+    K4 = k*(dudt(u + K3,v + J3,A,B) + D_u*(u_g - 2*u + u_d)/h**2)
+    J4 = k*(dvdt(u + K3,v + J3,A,B) + 10*(v_g - 2*v + v_d)/h**2)
+    u += (K1 + 2*K2 + 2*K3 + K4)/6
+    v += (J1 + 2*J2 + 2*J3 + J4)/6 
     plt.pause(k)
-    
     line1.set_ydata(u)
     line2.set_ydata(v)
